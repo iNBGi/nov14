38739d22f1317e225cef69b97031c53b4ca64730
iNBGi <132969754+iNBGi@users.noreply.github.com>
2023-12-06 13:16:46 +0800

    new with nav

--- react-main/app/screen/Profile/ProfileScreen.js
+++ react-main/app/screen/Profile/ProfileScreen.js
@@ -1,88 +1,323 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
-import { Button, Card, TextInput, Menu, Divider } from 'react-native-paper';
-import { AuthContext } from '../../../Context/authContext'; // Adjust the path
-import { loginStyle } from './login.style'; // Import the login style
-import axios from "axios";
+import React, { useContext, useState, useEffect } from 'react';
+import { FlatList, SafeAreaView, View, Text, Alert } from 'react-native';
+import { Button, Card, TextInput } from 'react-native-paper';
+import { AuthContext } from '../../../Context/authContext';
+import { loginStyle } from './login.style';
+import DropDownPicker from 'react-native-dropdown-picker';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const ProfileScreen = ({ navigation }) => {
-  const [weight, setWeight] = useState(''); // Define state for weight
-  const [height, setHeight] = useState(''); // Define state for height
-  const [name, setName] = useState('');
-  const { state, signOut } = useContext(AuthContext);
-  const [loading, setLoading] = useState(true);
-  const [visible, setVisible] = useState(false);
-  const [selectedItem, setSelectedItem] = useState(null);
-  const names = ['dekdek'];
+export const ProfileScreen = ({ navigation, route }) => {
+  const [weight, setWeight] = useState('');
+  const [height, setHeight] = useState('');
+  const [age, setAge] = useState('');
+  const [state, setState] = useContext(AuthContext);
+  const [bmi, setBMI] = useState(null);
+  const [selectedGoal, setSelectedGoal] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const userId = state.userId;
+  const [currentValue, setCurrentValue] = useState();
+  const [isDropdownDisabled, setIsDropdownDisabled] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [existingData, setExistingData] = useState(null);
+  const [isNewProfile, setIsNewProfile] = useState(false);
 
-  const openMenu = () => setVisible(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Fetch existing data based on userId
+        const { data } = await axios.get('http:10.0.2.2:3000/getProfile', {
+          params: { userId },
+        });
 
-  const closeMenu = () => setVisible(false);
+        if (data.success) {
+          const profileData = data.profileinfo;
+          if (profileData) {
+            setWeight(profileData.weight.toString());
+            setHeight(profileData.height.toString());
+            setAge(profileData.age.toString());
+            setBMI(profileData.bmi.toString());
+            setCurrentValue(profileData.goal);
+            setSelectedGoal(profileData.goal);
+            setIsDropdownDisabled(true);
+            setExistingData(profileData);
+            setIsNewProfile(false); // Set flag for existing profile
+          } else {
+            setIsNewProfile(true); // Set flag for new profile
+          }
+        }
+      } catch (error) {
+        // Handle error appropriately, check if it's a 404 indicating no profile found
+        if (error.response && error.response.status === 404) {
+          console.log('No profile found for userId:', userId);
+          setIsNewProfile(true); // Set flag for new profile
+        } else {
+          console.error('Error fetching profile data:', error);
+        }
+      }
+    };
 
-  const handleMenuItemPress = (item) => {
-    setSelectedItem(item);
-    closeMenu();
+    fetchData();
+  }, [userId]);
+
+  useEffect(() => {
+    // Calculate BMI whenever weight, height, or age changes
+    if (weight && height && age) {
+      calculateBMI();
+    }
+  }, [weight, height, age]);
+
+  const calculateBMI = () => {
+    const weightInKg = parseFloat(weight);
+    const heightInM = parseFloat(height) / 100;
+    const ageValue = parseInt(age);
+
+    if (weightInKg && heightInM && ageValue) {
+      const bmiValue = weightInKg / (heightInM * heightInM);
+      setBMI(bmiValue.toFixed(2));
+
+      let suggestedGoal = null;
+
+      if (bmiValue < 18.5) {
+        suggestedGoal = 'GW'; // Gain Weight
+      } else if (bmiValue >= 18.5 && bmiValue < 24.9) {
+        suggestedGoal = 'MW'; // Maintain Weight
+      } else {
+        suggestedGoal = 'LW'; // Lose Weight
+      }
+
+      setCurrentValue(suggestedGoal);
+      setSelectedGoal(suggestedGoal);
+
+      setIsDropdownDisabled(false);
+    }
   };
 
-  // Function to handle form submission
-  const handleSubmit = async () => {
-    // Your form submission logic here
-    // ...
-
-    // For example, you can navigate to the Login screen after successful registration
-    navigation.navigate('Profile');
+  const handleEdit = () => {
+    // Check if there are existing data
+    if (existingData) {
+      Alert.alert(
+        'Confirm Edit',
+        'Are you sure you want to edit your information?',
+        [
+          {
+            text: 'Cancel',
+            style: 'cancel',
+          },
+          {
+            text: 'OK',
+            onPress: () => {
+              setIsEditMode(true);
+              setIsDropdownDisabled(false);
+            },
+          },
+        ]
+      );
+    } else {
+      setIsEditMode(true);
+      setIsDropdownDisabled(false);
+    }
   };
+
+  const handleCancel = () => {
+    // Reset form values and disable editing
+    setWeight(existingData ? existingData.weight.toString() : '');
+    setHeight(existingData ? existingData.height.toString() : '');
+    setAge(existingData ? existingData.age.toString() : '');
+    setBMI(existingData ? existingData.bmi.toString() : '');
+    setCurrentValue(existingData ? existingData.goal : '');
+    setSelectedGoal(existingData ? existingData.goal : '');
+    setIsDropdownDisabled(true);
+    setIsEditMode(false);
+  };
+  const handleSaveOrUpdate = async () => {
+    try {
+      setLoading(true);
   
+      if (!validateInputs()) {
+        setLoading(false);
+        return;
+      }
+  
+      const requestData = {
+        userId: userId,
+        age: parseInt(age),
+        weight: parseInt(weight),
+        height: parseInt(height),
+        bmi: parseFloat(bmi),
+        goal: selectedGoal,
+      };
+  
+      console.log('Request Data:', requestData);
+  
+      const { data } = await axios.post('http:10.0.2.2:3000/bmiposting', requestData);
+  
+      if (data.success === true) {
+        alert(data && data.message);
+        console.log('Information Submitted/Updated:', requestData);
+  
+        // Set isEditMode to true and fetch data again to recheck if there is information
+        setIsEditMode(true);
+        fetchData();
+      }
+    } catch (error) {
+      alert(error.response.data.message);
+      setLoading(false);
+      console.log(error);
+    } finally {
+      // Reset edit mode after saving or updating
+      setIsEditMode(false);
+    }
+  };
+
+  const validateInputs = () => {
+    const parsedAge = parseInt(age, 10);
+    const parsedHeight = parseInt(height, 10);
+    const parsedWeight = parseInt(weight, 10);
+    const parsedBMI = parseFloat(bmi);
+
+    if (isNaN(parsedAge) || isNaN(parsedHeight) || isNaN(parsedWeight) || isNaN(parsedBMI)) {
+      Alert.alert('Error', 'Please enter valid numeric values for Age, Height, Weight, and BMI');
+      return false;
+    }
+
+    if (!selectedGoal) {
+      Alert.alert('Error', 'Please choose a goal');
+      return false;
+    }
+
+    return true;
+  };
+
+  const items = [
+    { label: 'Gain Weight', value: 'GW' },
+    { label: 'Maintain Weight', value: 'MW' },
+    { label: 'Lose Weight', value: 'LW' },
+  ];
+
+  const handleLogout = async () => {
+    try {
+      await AsyncStorage.removeItem('@auth:token');
+      await AsyncStorage.removeItem('@auth:userId');
+      setState((prevState) => ({ ...prevState, token: null }));
+      navigation.navigate('Login');
+    } catch (error) {
+      console.error('Error logging out:', error);
+    }
+  };
 
   return (
-    <SafeAreaView>
-      <View>
-      <Card>
-    <Card.Content>
-      <Text style={{ fontSize: 20, textAlign: 'center', marginVertical: 20 }}>
-        Welcome! {state && state.firstname ? state.firstname : 'Guest'}!
-      </Text>
-    </Card.Content>
-  </Card>
-        <Card.Title />
-        <Card.Content>
-          {/* TextInput components with consistent styling */}
-          <TextInput
-            label="Weight"
-            value={weight}
-            onChangeText={(text) => setWeight(text)}
-          />
-          <TextInput
-            label="Height"
-            value={height}
-            onChangeText={(text) => setHeight(text)}
-          />
+    <SafeAreaView style={{ flex: 1 }}>
+      <FlatList
+        contentContainerStyle={{ flexGrow: 1 }}
+        data={['dummy']}
+        keyExtractor={() => 'dummyKey'}
+        renderItem={() => (
+          <View style={{ flex: 1, marginBottom: 20 }}>
+            <Text style={{ fontSize: 20, textAlign: 'center', marginVertical: 20 }}>
+              Welcome {state.firstname}!
+            </Text>
+            <Card.Title />
+            <Card.Content>
+            <TextInput
+  label="Weight"
+  value={weight}
+  onChangeText={(text) => setWeight(text)}
+  editable={isEditMode || isNewProfile} // Allow editing in edit mode or for a new profile
+/>
+<TextInput
+  label="Height"
+  value={height}
+  onChangeText={(text) => setHeight(text)}
+  editable={isEditMode || isNewProfile}
+/>
+<TextInput
+  label="Age"
+  value={age}
+  onChangeText={(text) => setAge(text)}
+  editable={isEditMode || isNewProfile}
+/>
 
-          <Button
-            mode="contained"
-            style={loginStyle.loginButton}
-            onPress={handleSubmit}
-          >
-            Input
-          </Button>
 
-          {/* Dropdown menu */}
-          <Menu
-            visible={visible}
-            onDismiss={closeMenu}
-            anchor={<Button onPress={openMenu}>Suggestion</Button>}
-          >
-            {names.map((item, index) => (
-              <Menu.Item
-                key={index}
-                onPress={() => handleMenuItemPress(item)}
-                title={item}
-              />
-            ))}
-          </Menu>
+              {bmi && (
+                <View>
+                  <Text style={{ fontSize: 18, marginTop: 10 }}>
+                    Your BMI: {bmi}
+                  </Text>
+                </View>
+              )}
 
-        </Card.Content>
-      </View>
+              <View style={{ padding: 30 }}>
+                <DropDownPicker
+                  items={items}
+                  open={isOpen}
+                  disabled={isDropdownDisabled || !isEditMode}
+                  setOpen={() => setIsOpen(!isOpen)}
+                  value={currentValue}
+                  setValue={(val) => {
+                    setCurrentValue(val);
+                    setSelectedGoal(val);
+                  }}
+                  maxHeight={200}
+                  autoScroll
+                  placeholder="Choose Goal"
+                />
+              </View>
+
+              {isEditMode ? (
+                <View>
+                  <Button
+                    mode="contained"
+                    style={[loginStyle.loginButton, { marginBottom: 20 }]}
+                    onPress={handleSaveOrUpdate}
+                  >
+                    Save Updated Information
+                  </Button>
+
+                  <Button
+                    mode="contained"
+                    style={loginStyle.loginButton}
+                    onPress={handleCancel}
+                  >
+                    Cancel
+                  </Button>
+                </View>
+              ) : (
+                <>
+                  {existingData && (
+                    <Button
+                      mode="contained"
+                      style={[loginStyle.loginButton, { marginBottom: 20 }]}
+                      onPress={handleEdit}
+                    >
+                      Edit Information
+                    </Button>
+                  )}
+
+                  {isNewProfile && (
+                    <Button
+                      mode="contained"
+                      style={[loginStyle.loginButton, { marginBottom: 20 }]}
+                      onPress={handleSaveOrUpdate}
+                    >
+                      Save Information
+                    </Button>
+                  )}
+                </>
+              )}
+
+              <Button
+                mode="contained"
+                style={loginStyle.loginButton}
+                onPress={handleLogout}
+              >
+                Logout
+              </Button>
+            </Card.Content>
+          </View>
+        )}
+      />
     </SafeAreaView>
   );
 };
--- react-main/app/screen/Sensor/squatsensor.js
+++ react-main/app/screen/Sensor/squatsensor.js
@@ -1,63 +1,92 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import HumanPose from 'react-native-human-pose';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 export const Squatsensor = () => {
   const [noOfSquats, setNoOfSquats] = useState(0);
-  const [hasSit, setHasSit] = useState(false);
-  const [hasStand, setHasStand] = useState(false);
-
-  const calculateAngle = (joint1, joint2, joint3) => {
-    const radians = Math.atan2(joint3.y - joint2.y, joint3.x - joint2.x) - Math.atan2(joint1.y - joint2.y, joint1.x - joint2.x);
-    let angle = Math.abs((radians * 180.0) / Math.PI);
-
-    // Ensure angle is within 0 to 360 degrees
-    if (angle > 180) {
-      angle = 360 - angle;
-    }
-
-    return angle;
-  };
+  const [isSquatting, setIsSquatting] = useState(false);
+  const [message, setMessage] = useState('');
+  const [isStarted, setIsStarted] = useState(false);
+  const [countdown, setCountdown] = useState(0);
+  const [status, setStatus] = useState('unknown');
+  const isCountedRef = useRef(false); // Ref to track counting status
 
   const onPoseDetected = (pose) => {
-    const leftHip = pose[0]?.pose?.leftHip;
-    const leftKnee = pose[0]?.pose?.leftKnee;
-    const leftAnkle = pose[0]?.pose?.leftAnkle;
-
-    const rightHip = pose[0]?.pose?.rightHip;
-    const rightKnee = pose[0]?.pose?.rightKnee;
-    const rightAnkle = pose[0]?.pose?.rightAnkle;
-
-    const minConfidence = 0.5; // Adjust the confidence level as needed
-
+    const leftHipY = pose[0]?.pose?.leftHip?.y;
+    const leftKneeY = pose[0]?.pose?.leftKnee?.y;
+    const leftAnkleY = pose[0]?.pose?.leftAnkle?.y;
+  
     if (
-      leftHip?.confidence > minConfidence &&
-      leftKnee?.confidence > minConfidence &&
-      leftAnkle?.confidence > minConfidence &&
-      rightHip?.confidence > minConfidence &&
-      rightKnee?.confidence > minConfidence &&
-      rightAnkle?.confidence > minConfidence
+      pose[0]?.pose?.leftHip?.confidence > 0.6 &&
+      pose[0]?.pose?.leftKnee?.confidence > 0.6 &&
+      pose[0]?.pose?.leftAnkle?.confidence > 0.6
     ) {
-      const leftHipKneeAnkleAngle = calculateAngle(leftHip, leftKnee, leftAnkle);
-      const rightHipKneeAnkleAngle = calculateAngle(rightHip, rightKnee, rightAnkle);
-
-      if (leftHipKneeAnkleAngle > 160 && rightHipKneeAnkleAngle > 160) {
-        // Adjust the threshold angle as needed
-        setHasSit(true);
-        setHasStand(false);
+      const verticalDistanceHipKnee = Math.abs(leftHipY - leftKneeY);
+      const verticalDistanceKneeAnkle = Math.abs(leftKneeY - leftAnkleY);
+  
+      // Check for high knee position
+      if (verticalDistanceHipKnee > 200) {
+        setIsSquatting(false);
+        setMessage('High Knee - Invalid Count');
+        setStatus('improper');
+        return;
       }
-
-      if (hasSit && (leftHipKneeAnkleAngle < 140 || rightHipKneeAnkleAngle < 140)) {
-        // Adjust the threshold angle as needed
-        setHasStand(true);
-        setHasSit(false);
+  
+      // Check for half squat position
+      if (verticalDistanceKneeAnkle > 200) {
+        setIsSquatting(false);
+        setMessage('Half Squat - Invalid Count');
+        setStatus('improper');
+        return;
       }
-    }
+  
+      const verticalDistance = Math.abs(leftHipY - leftAnkleY);
+  
+      if (verticalDistance < 300) {
+        if (!isSquatting) {
+          setIsSquatting(true);
+          setStatus('proper');
+          setMessage('');
+          isCountedRef.current = false; // Reset the ref when starting a squat
+        }
+      } else if (isSquatting && !isCounted) {
+        setIsSquatting(false);
+        setStatus('improper');
+        setMessage('');
+        setNoOfSquats(noOfSquats + 1);
+        isCountedRef.current = true; // Set the ref to true after counting a squat
+      }
+    };
   };
 
+
   useEffect(() => {
-    setNoOfSquats(hasStand ? noOfSquats + 1 : noOfSquats);
-  }, [hasStand]);
+    if (isStarted && isSquatting && !isCountedRef.current) {
+      setNoOfSquats(noOfSquats + 1);
+      isCountedRef.current = true; // Set the ref to true after counting a squat
+    }
+  }, [isSquatting, isStarted]);
+
+  const handleStart = () => {
+    setIsStarted(true);
+    setNoOfSquats(0);
+    setCountdown(5);
+
+    const countdownInterval = setInterval(() => {
+      setCountdown((prevCountdown) => prevCountdown - 1);
+    }, 1000);
+
+    setTimeout(() => {
+      clearInterval(countdownInterval);
+    }, 5000);
+  };
+
+  const handleStop = () => {
+    setIsStarted(false);
+    setIsSquatting(false);
+    setMessage('');
+    setStatus('unknown');
+  };
 
   return (
     <View style={{ flex: 1 }}>
@@ -69,8 +98,8 @@ export const Squatsensor = () => {
         enableSkeleton={true}
         flipHorizontal={false}
         isBackCamera={false}
-        color={'255, 0, 0'}
-        mode={'single'} // Set mode to 'single'
+        color={'0, 255, 0'} // Initial color is red
+        mode={'single'}
         onPoseDetected={onPoseDetected}
       />
       <Text
@@ -84,11 +113,32 @@ export const Squatsensor = () => {
           backgroundColor: 'white',
           padding: 10,
           fontSize: 20,
+          color: status === 'proper' ? 'green' : 'red',
         }}>
-        No of Squats: {noOfSquats}
+        {isStarted && countdown > 0 ? `Starting in ${countdown}` : `No of Squats: ${noOfSquats}`}
       </Text>
+      {isSquatting && status !== 'unknown' && (
+        <Text
+          style={{
+            position: 'absolute',
+            top: 100,
+            left: 0,
+            right: 0,
+            textAlign: 'center',
+            fontSize: 70,
+            color: status === 'proper' ? 'green' : 'red',
+            fontWeight: 'bold'
+          }}>
+          {status}
+        </Text>
+      )}
+      <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginTop: 20 }}>
+        <Button title="Start" onPress={handleStart} disabled={isStarted} />
+        <Button title="Stop" onPress={handleStop} disabled={!isStarted} />
+      </View>
     </View>
   );
 };
 
 export default Squatsensor;
+
